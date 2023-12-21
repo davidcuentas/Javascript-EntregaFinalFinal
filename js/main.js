b9d60e6231f2b12ef98f93d9653f91e22109363e
David Cuentas <71470023+davidcuentas@users.noreply.github.com>
2023-12-20 23:28:47 -0500

    fetch

--- js/main.js
+++ js/main.js
@@ -1,169 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const datosProductos = [
-    {
-      id: 1,
-      nombre: "Base jabón blanca",
-      precio: 41000,
-      categoria: "Base",
-      imagen: "images/base-jabón-blanca.jpg",
-    },
 
-    {
-      id: 2,
-      nombre: "Base jabón transparente",
-      precio: 43000,
-      categoria: "Base",
-      imagen: "images/base-jabón-transparente.jpg",
-    },
-
-    {
-      id: 3,
-      nombre: "Base acondicionador",
-      precio: 56600,
-      categoria: "Base",
-      imagen: "images/base-acondicionador.jpg",
-    },
-
-    {
-      id: 4,
-      nombre: "Base crema corporal",
-      precio: 56600,
-      categoria: "Base",
-      imagen: "images/base-crema-corporal.jpg",
-    },
-
-    {
-      id: 5,
-      nombre: "Base shampoo",
-      precio: 54600,
-      categoria: "Base",
-      imagen: "images/base-shampoo.jpg",
-    },
-
-    {
-      id: 6,
-      nombre: "Color amarillo",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-amarillo.jpg",
-    },
-
-    {
-      id: 7,
-      nombre: "Color azul",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-azul.jpg",
-    },
-
-    {
-      id: 8,
-      nombre: "Color cyan",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-cyan.jpg",
-    },
-
-    {
-      id: 9,
-      nombre: "Color café",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-café.jpg",
-    },
-
-    {
-      id: 10,
-      nombre: "Color rojo",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-rojo.jpg",
-    },
-
-    {
-      id: 11,
-      nombre: "Color fucsia",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-fucsia.jpg",
-    },
-
-    {
-      id: 12,
-      nombre: "Color negro",
-      precio: 8300,
-      categoria: "Pigmento",
-      imagen: "images/color-negro.jpg",
-    },
-
-    {
-      id: 13,
-      nombre: "Caléndula",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/caléndula.jpg",
-    },
-
-    {
-      id: 14,
-      nombre: "Lavanda",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/lavanda.jpg",
-    },
-
-    {
-      id: 15,
-      nombre: "Manzanilla",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/manzanilla.jpg",
-    },
-
-    {
-      id: 16,
-      nombre: "Menta",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/menta.jpg",
-    },
-
-    {
-      id: 17,
-      nombre: "Romero",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/romero.jpg",
-    },
-
-    {
-      id: 18,
-      nombre: "Rosas",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/rosas.jpg",
-    },
-
-    {
-      id: 19,
-      nombre: "Sal del Himalaya",
-      precio: 4500,
-      categoria: "Ingredientes",
-      imagen: "images/sal-del-himalaya.jpg",
-    },
-  ];
-
-
+  let productos = [];
   const carrito = obtenerCarritoDesdeLocalStorage() || [];
   const listaCarrito = document.getElementById('lista-carrito');
   const totalCarritoElement = document.getElementById('total-carrito');
   const carritoContainer = document.getElementById('carrito');
 
+  fetch("json/data.json")
+    .then(response => response.json())
+    .then(data => {
+      productos = data;
+      mostrarProductos(productos)
+    })
+
   function mostrarProductos(productos) {
     const contenedor = document.getElementById('contenedor-productos');
     contenedor.innerHTML = '';
-
     productos.forEach(producto => {
       const elementoProducto = document.createElement('div');
       elementoProducto.classList.add('producto');
@@ -180,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function () {
   function actualizarCarrito() {
     listaCarrito.innerHTML = '';
     let totalCarrito = 0;
-
     carrito.forEach((item, index) => {
       const listItem = document.createElement('li');
       listItem.innerHTML = `${item.nombre} - $${item.precio}
@@ -189,7 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
       totalCarrito += item.precio;
       compraRojo.style.display = 'block';
     });
-
     totalCarritoElement.textContent = totalCarrito;
     guardarCarritoEnLocalStorage();
   }
@@ -206,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
   window.ocultarCarrito = ocultarCarrito;
 
   window.agregarAlCarrito = function (idProducto) {
-    const productoSeleccionado = datosProductos.find(producto => producto.id === idProducto);
+    const productoSeleccionado = productos.find(producto => producto.id === idProducto);
     carrito.push(productoSeleccionado);
     actualizarCarrito();
   };
@@ -226,12 +77,12 @@ document.addEventListener('DOMContentLoaded', function () {
   };
 
   window.mostrarProductosPorCategoria = function (categoria) {
-    const productosPorCategoria = datosProductos.filter(producto => producto.categoria === categoria);
+    const productosPorCategoria = productos.filter(producto => producto.categoria === categoria);
     mostrarProductos(productosPorCategoria);
   };
 
   window.mostrarTodosLosProductos = function () {
-    mostrarProductos(datosProductos);
+    mostrarProductos(productos);
   };
 
   window.mostrarResumenCompra = function () {
@@ -241,18 +92,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalContenido = document.querySelector('.modal-contenido');
     resumenCarrito.innerHTML = '';
     let totalResumen = 0;
-
     carrito.forEach(item => {
       const listItem = document.createElement('li');
       listItem.textContent = `${item.nombre} - $${item.precio}`;
       resumenCarrito.appendChild(listItem);
       totalResumen += item.precio;
     });
-
     totalResumenElement.textContent = totalResumen;
-
     modal.style.display = 'block';
-
     window.onclick = function (event) {
       if (event.target === modal) {
         cerrarModal();
@@ -280,7 +127,6 @@ document.addEventListener('DOMContentLoaded', function () {
         toast.addEventListener('mouseleave', Swal.resumeTimer)
       }
     })
-    
     Toast.fire({
       icon: 'success',
       title: '¡Su compra ha sido realizada!'
@@ -296,22 +142,19 @@ document.addEventListener('DOMContentLoaded', function () {
   function guardarCarritoEnLocalStorage() {
     localStorage.setItem('carrito', JSON.stringify(carrito));
   }
-
-  mostrarProductos(datosProductos);
+  mostrarProductos(productos);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var botonFlotante = document.getElementById('boton-flotante');
   var footer = document.querySelector('footer');
-
-  window.addEventListener('scroll', function() {
-      var scrollHeight = window.scrollY + window.innerHeight;
-      var footerOffset = footer.offsetTop;
-
-      if (scrollHeight > footerOffset) {
-          botonFlotante.style.bottom = (scrollHeight - footerOffset + 20) + 'px';
-      } else {
-          botonFlotante.style.bottom = '20px';
-      }
+  window.addEventListener('scroll', function () {
+    var scrollHeight = window.scrollY + window.innerHeight;
+    var footerOffset = footer.offsetTop;
+    if (scrollHeight > footerOffset) {
+      botonFlotante.style.bottom = (scrollHeight - footerOffset + 20) + 'px';
+    } else {
+      botonFlotante.style.bottom = '20px';
+    }
   });
 });
